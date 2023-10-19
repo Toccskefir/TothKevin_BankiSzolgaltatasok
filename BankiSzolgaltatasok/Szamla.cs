bfc21aefb564a4ec6bad8df457c68f57de70ee07
Tóth Kevin <tothkevin0716@gmail.com>
2023-10-19 09:06:49 +0200

    Szamla class

---
+++ BankiSzolgaltatasok/Szamla.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+    public abstract class Szamla : BankiSzolgaltatas
+    {
+        private int aktualisEgyenleg;
+
+        protected int AktualisEgyenleg { get => aktualisEgyenleg; }
+
+        public Szamla(Tulajdonos tulajdonos) : base(tulajdonos) { }
+
+        public void Befizet(int osszeg)
+        {
+            this.aktualisEgyenleg += osszeg;
+        }
+
+        public abstract bool Kivesz(int osszeg);
+    }
+}
