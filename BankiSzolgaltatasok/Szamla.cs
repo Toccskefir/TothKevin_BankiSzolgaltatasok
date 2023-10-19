1fbdd13f5eb210deee2b752cac0ab1f80948e8f0
Tóth Kevin <tothkevin0716@gmail.com>
2023-10-19 10:33:44 +0200

    Szamla class UjKartya method

--- BankiSzolgaltatasok/Szamla.cs
+++ BankiSzolgaltatasok/Szamla.cs
@@ -20,5 +20,10 @@ namespace BankiSzolgaltatasok
         }
 
         public abstract bool Kivesz(int osszeg);
+
+        public Kartya UjKartya(string kartyaSzam)
+        {
+            return new Kartya(this.Tulajdonos, this, kartyaSzam);
+        }
     }
 }
